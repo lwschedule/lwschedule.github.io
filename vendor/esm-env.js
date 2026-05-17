c6fe70cebdce7ee0d6a11514be005d90821dbdd8
sdphadke <96030081+sdphadke@users.noreply.github.com>
2026-05-17 15:58:17 -0700

    changes

---
+++ vendor/esm-env.js
@@ -0,0 +1,5 @@
+// /vendor/esm-env.js
+// Browser shim for the esm-env package.
+// In a browser context BROWSER is always true, DEV is always false.
+export const BROWSER = true;
+export const DEV = false;
